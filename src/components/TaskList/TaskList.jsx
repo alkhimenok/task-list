4c208d7c6e9cbcc79034b9d4ef0063d9707b1cc8
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-01-22 21:14:16 +0300

    add: icons

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -6,6 +6,11 @@ const Form = ({ blockClass, taskList }) => {
 			{taskList.map(task => (
 				<li className="task-list__item" key={task.id}>
 					{task.title}
+					<form className="task-list__form">
+						<button className="task-list__btn" name='important'><i className='icon-important'></i></button>
+						<button className="task-list__btn" name='completed'><i className='icon-completed'></i></button>
+						<button className="task-list__btn" name='delete'><i className='icon-delete'></i></button>
+					</form>
 				</li>
 			))}
 		</ul>
