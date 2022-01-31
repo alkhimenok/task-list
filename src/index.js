06d53c17707a57a73dc9183da82afed3d4109879
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-01 00:38:01 +0300

    add: styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-import { Fonts, Icons, Reset } from './styledApp'
-// import './index.css'
+import './index.css'
 
-ReactDOM.render(
-	<>
-		<Reset />
-		<Fonts />
-		<Icons />
-		<App />
-	</>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
