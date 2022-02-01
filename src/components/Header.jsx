7de8cf7fe6cffa2092ed2ecd2cd3cfb9c60b0b48
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-01 20:48:26 +0300

    add(Header): auto scrol

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from './common/Container'
 import Body from './common/Body'
 import Image from './common/Image'
@@ -6,15 +6,30 @@ import Switch from './common/Switch'
 
 const Header = () => {
 	const [switchClassName, setSwitchClassName] = useState('header__switch')
-	const backgroundSrcList = ['backgrounds/1.jpg', 'backgrounds/2.jpg', 'backgrounds/3.jpg', 'backgrounds/4.jpg']
+	const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0)
+
+	const backgroundSrcList = ['backgrounds/1.jpg', 'backgrounds/2.jpg', 'backgrounds/3.jpg']
 
 	const handleSwitchClick = () => {
 		setSwitchClassName(prev => (prev.includes('switch--active') ? 'header__switch' : 'header__switch switch--active'))
 	}
 
+	useEffect(() => {
+		let isNext = true
+
+		setInterval(() => {
+			setCurrentBackgroundIndex(prev => {
+				if (prev === backgroundSrcList.length - 1) isNext = false
+				if (prev === 0) isNext = true
+
+				return isNext ? prev + 1 : prev - 1
+			})
+		}, 10000)
+	}, [])
+
 	return (
 		<header className="header">
-			<div className="header__background">
+			<div className="header__background" style={{ transform: `translate(${-100 * currentBackgroundIndex}%, 0)` }}>
 				{backgroundSrcList.map((src, i) => (
 					<Image parentBlockClass={'header__img'} src={src} alt={'background'} key={i} />
 				))}
