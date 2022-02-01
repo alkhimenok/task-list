525afc143ba11247cdc634dd2379faf7d5999810
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-02 00:41:43 +0300

    add(List): task list

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = ({ parentBlockClass }) => {
+const Form = ({ parentBlockClass, handleSubmit }) => {
+	const [task, setTask] = useState('')
+
 	return (
-		<form className={[parentBlockClass, 'form'].join(' ')}>
+		<form
+			className={[parentBlockClass, 'form'].join(' ')}
+			onSubmit={e => {
+				if (task !== '') {
+					handleSubmit(task)
+					setTask('')
+				} else {
+          // showModal()
+        }
+
+				e.preventDefault()
+			}}>
 			<fieldset className="form__item">
-				<input className="form__input" id="taskInput" type="text" placeholder=" " />
+				<input
+					className="form__input"
+					id="taskInput"
+					type="text"
+					placeholder=" "
+					value={task}
+					onChange={e => setTask(e.target.value)}
+				/>
 				<label className="form__label" htmlFor="taskInput">
 					Enter your task
 				</label>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Body from './common/Body'
 import Container from './common/Container'
 import Form from './Form'
+import List from './List'
 
 const Main = () => {
+	const [taskList, setTaskList] = useState(['hello','hello','hello','hello','hello','hello','hello','hello','hello'])
+
+	const handleTaskSubmit = task => setTaskList([task, ...taskList])
+
+	const handleTaskAction = e => {
+		console.log(e.target)
+
+		e.preventDefault()
+	}
+
 	return (
 		<main className="main">
 			<Container parentBlockClass={'main__container'}>
 				<Body parentBlockClass={'main__body'}>
-					<Form parentBlockClass={'main__form'} />
+					<Form parentBlockClass={'main__form'} handleSubmit={handleTaskSubmit} />
+					<List parentBlockClass={'main__list'} taskList={taskList} handleClick={handleTaskAction} />
 				</Body>
 			</Container>
 		</main>
