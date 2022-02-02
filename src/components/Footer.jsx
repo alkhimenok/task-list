daa489224ae20d586d98681c409b9eca5eadccb7
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-02 03:47:55 +0300

    add: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Body from './common/Body'
+import Container from './common/Container'
 
 const Footer = () => {
-	return <footer>hello</footer>
+	return (
+		<footer className="footer">
+			<Container parentBlockClass={'footer__container'}>
+				<Body parentBlockClass={'footer__body'}>
+					<form className="footer__form">
+						<button className="footer__btn footer__btn--completed">do everything</button>
+						<button className="footer__btn footer__btn--important">mark all as important</button>
+						<button className="footer__btn footer__btn--delete">delete everything</button>
+					</form>
+				</Body>
+			</Container>
+		</footer>
+	)
 }
 
 export default Footer
